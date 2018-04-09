6a73d84441f7600333082b6563ca467be88583d1
bhuard <phymeso@gmail.com>
2018-04-09 16:18:32 +0200

    Change driver and run AWG task for transfer pulse loop task

--- exopy_hqc_legacy/tasks/tasks/instr/run_awg.py
+++ exopy_hqc_legacy/tasks/tasks/instr/run_awg.py
@@ -11,7 +11,7 @@
 """
 import logging
 
-from atom.api import (Unicode, set_default)
+from atom.api import (Unicode, Enum, set_default)
 
 from exopy.tasks.api import (InstrumentTask, validators)
 
@@ -23,19 +23,32 @@ class RunAWGTask(InstrumentTask):
     #: Switch to choose the AWG run mode: on or off
     switch = Unicode('Off').tag(pref=True, feval=validators.SkipLoop())
     database_entries = set_default({'output': 0})
+    mode = Enum('CONTINUOUS', 'TRIGGER', 'GATED','SEQUENCE').tag(pref=True)
 
     def perform(self, switch=None):
         """Default interface behavior.
 
         """
+        self.driver.run_mode = self.mode        
+        
         if switch is None:
             switch = self.format_and_eval_string(self.switch)
 
+        running_state = self.driver.running
+
         if switch == 'On' or switch == 1:
+            if running_state != '0 : Instrument has stopped':
+                print('WARNING: AWG already running')
+            for i in range(4):
+                ch = self.driver.get_channel(i+1)
+                if ch.channel_sequence:
+                    ch.output_state = 'ON'
             self.driver.running = 1
             self.write_in_database('output', 1)
         else:
             self.driver.running = 0
+            if running_state != '2 : Intrument is running':
+                print('WARNING: AWG already stopped or waiting for a trigger')
             self.write_in_database('output', 0)
         log = logging.getLogger(__name__)
         msg = 'AWG running state OK'
